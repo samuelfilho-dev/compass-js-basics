ddebb892b67bbe1b10157c5d5ebdfe5b349266e6
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-05 12:14:48 -0300

    add variables file

---
+++ scripts/variables.js
@@ -0,0 +1,14 @@
+// Declaração de Variavel
+let name = 'Mosh'
+console.log(name);
+
+let firstName
+
+// ***** Regras Das Variaveis ******
+// Não pode ter palavra reservada
+// Deve ter nomes significativos
+// Não pode iniciar com um número
+// Não pode conter espaço ou hífen(-)
+// As variaveis usam estilo de nomenclatura camelCase
+// As Variaveis são Case-Sensitive
+// Declarar a variavel linha por linha
