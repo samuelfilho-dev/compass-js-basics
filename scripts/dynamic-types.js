ca42bb7c0fcf3655ce476395af490c98bea5de17
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-05 12:45:37 -0300

    add dynamic-types file

---
+++ scripts/dynamic-types.js
@@ -0,0 +1,13 @@
+let name = 'Mosh'; // Declarado Como String
+let age = 30; 
+let isApproved = true; 
+let firstName = undefined; 
+let selectedColor = null; 
+
+name = 1 // Declarado com Number
+age = 30.1; 
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isApproved);
+console.log(typeof firstName);
+console.log(typeof lastName);
--- scripts/primitives-types.js
+++ scripts/primitives-types.js
@@ -1,5 +1,5 @@
 let name = 'Mosh'; // String Literal
 let age = 30; // Number
 let isApproved = true; // Boolean Literal
-let firsName = undefined; // Undefined
+let firstName = undefined; // Undefined
 let lastName = null; // Null
