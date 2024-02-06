3af2517d28b677f287dc69445c3820571946afa9
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-06 15:53:04 -0300

    add if-else file

---
+++ scripts/if-else.js
@@ -0,0 +1,11 @@
+let hour = 23;
+
+if (hour >= 6 && hour < 12) {
+    console.log("Good Moring");
+} 
+else if (hour >= 12 && hour < 18) {
+    console.log("Good Afternoon")
+} else {
+    console.log("Good Evening");
+}
+
