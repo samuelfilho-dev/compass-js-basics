ad1fb86a99eb2ea3edb8d5bf6583ee017cb6f47b
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-06 12:50:54 -0300

    add bitwise Operetor

--- scripts/operators.js
+++ scripts/operators.js
@@ -55,6 +55,21 @@ let currentColor = userColor || defaultColor;
 
 console.log(currentColor);
 
+// Operadores Bitwise
+
+console.log(1 | 2) // Bitwise OR
+console.log(1 & 2) // Bitwise AND
+
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
+
+let myPermission = 0;
+myPermission = myPermission | readPermission | writePermission | executePermission
+
+let message = (myPermission & readPermission) ? 'yes' : 'no';
+console.log(message);
+
 /*
     Falsy (false)
 
