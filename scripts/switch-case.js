fa527f5d9ed454a3619cd5e9a683728bdb94c673
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-06 16:15:09 -0300

    add swich case file

---
+++ scripts/switch-case.js
@@ -0,0 +1,12 @@
+let role = 'moderator';
+
+switch (role) {
+    case 'guest':
+        console.log('Guest User');
+        break;
+    case 'moderator':
+        console.log("Moderator User");
+        break;
+    default:
+        console.log("Unknown User");
+}
