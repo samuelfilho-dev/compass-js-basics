4b944b40249faff1f55fcbb4471d982b576bab3f
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-06 17:00:46 -0300

    add exe max of two numbers

---
+++ scripts/execicios/exe-max-two-numbers.js
@@ -0,0 +1,9 @@
+function maxNumber(numberOne, numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log("Number one is Bigger than Number Two");
+    } else {
+        console.log("Number Two is Bigger than Number One");
+    }
+}
+
+maxNumber(10,5);
