f4781770cf194e99a5520b66318be98f988408da
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-02-06 18:48:19 -0300

    add intro objects file

---
+++ scripts/objects/intro.js
@@ -0,0 +1,15 @@
+
+// Object-Oriented Programming (OOP)
+const circle = {
+    radius: 1,
+    locaciton: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
+    },
+};
+
+circle.draw(); // Method
